d4332704e257273c2671f35e6b7d5390829bef27
sungchun12 <sungwonchung3@gmail.com>
2019-06-11 20:11:23 +0000

    quick and dirty python code to test jupyter

--- jupyter_templates/template.py
+++ jupyter_templates/template.py
@@ -13,13 +13,15 @@ import sys
 import logging
 
 import numpy as np
+
 # import scipy as sp
 # import sklearn
 # import statsmodels.api as sm
 # from statsmodels.formula.api import ols
 
 # import pandas as pd
-import modin.pandas as pd
+import pandas as pd
+
 pd.set_option("display.max_rows", 120)
 pd.set_option("display.max_columns", 120)
 
@@ -38,8 +40,11 @@ pd.set_option("display.max_columns", 120)
 
 
 #%%
-frame_data = np.random.randint(0, 100, size=(2**10, 2**8))
+frame_data = np.random.randint(0, 100, size=(2 ** 10, 2 ** 8))
 df = pd.DataFrame(frame_data)
 df.head()
+
+#%%
+frame_data
 
 #%%
